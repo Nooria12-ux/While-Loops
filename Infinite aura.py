c2850e11b53812d5c63739dbb4b0ef6326c4a329
Nooria <samihamaysoonnooria@gmail.com>
2025-02-07 16:12:39 +0600

    first commit

---
+++ Infinite aura.py
@@ -0,0 +1,5 @@
+i = 0
+count = 0
+while i < 1:
+    count = count + 1
+    print(f"My aura = {count}+")
---
+++ sum of natural numbers.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number: "))
+i = 0
+sum = 0
+
+while i< num:
+    sum = sum + i
+    print(f"for i = {i} == {sum}")
+    i = i+1
+    
